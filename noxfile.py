86fe0ad22a75b553bbf9e678448ac12849784d19
James Fitzgerald <james@jawfish.dev>
2023-05-15 16:19:05 -0400

    build: improve noxfile

--- noxfile.py
+++ noxfile.py
@@ -2,10 +2,22 @@
 import nox
 
 
-@nox.session(python=["3.11.3"])
-def tests(session) -> None:
-    session.run("poetry", "run", "pre-commit", "run", "--all-files", external=True)
+@nox.session(python=["3.11"])
+def precommit(session):
+    session.run("pre-commit", "run", "--all-files", external=True)
+
+
+@nox.session(python=["3.11"])
+def lint(session) -> None:
     session.run("poetry", "run", "black", "app", external=True)
     session.run("poetry", "run", "ruff", "--fix", "app", external=True)
-    session.run("poetry", "run", "mypy", "app", external=True)
+
+
+@nox.session(python=["3.11"])
+def test(session) -> None:
     session.run("poetry", "run", "pytest", external=True)
+
+
+@nox.session(python=["3.11"])
+def types(session) -> None:
+    session.run("poetry", "run", "mypy", "app", external=True)
